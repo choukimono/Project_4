d4a210ba9304afc04492d9af62ca810d1b33c653
Ethel Villeneuve <ethel.villeneuve@gmail.com>
2017-11-19 23:31:14 +0100

    Revert "Revert "program""
    This reverts commit 0deffba3815f753df9a64f811549ce851d1e5e9f.

---
+++ Program/Program/functions.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+//  creates a random number in [0, dim)
+double random_number(const int dim);
+//  creates a random number in [0, 1]
+double small_random_number(void);
+//  creates a random number in [0, infinity)
+double big_random_number(void);
+
+
---
+++ Program/Program/unit-tests.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+int run_unittest(int argc, const char* argv[]);
